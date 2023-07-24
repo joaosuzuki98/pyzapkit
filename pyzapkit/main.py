99cc9ed92de6dc5d6474acf2791f88611fcddace
João Suzuki <joaosuzuki98@outlook.com>
2023-07-23 22:54:14 -0300

    Added method to send messages

--- pyzapkit/main.py
+++ pyzapkit/main.py
@@ -1,12 +1,21 @@
 """Send WhatsApp messages"""
 import os
 import platform
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 
-class Zap():
-    def __init__(self, profile='Default', headless=True):
+# TODO adicionar os tipos
+class Pyzap():
+    def __init__(
+            self, profile: str = 'Default', headless: bool = True) -> None:
+
         self.profile = profile
         self.headless = headless
         op_sys = platform.system()
@@ -31,20 +40,52 @@ class Zap():
             home_path_windows = os.environ['USERPROFILE']
 
             options.add_argument(
-                {f'user-data-dir={home_path_windows}'
-                 '\\AppData\\Local\\Google\\Chrome\\User Data'}
+                f'user-data-dir={home_path_windows}'
+                '\\AppData\\Local\\Google\\Chrome\\User Data'
             )
             service = Service(executable_path='./driver/chromedriver_win64')
 
         options.add_argument(f'--profile-directory={self.profile}')
 
-        driver = webdriver.Chrome(
+        self.driver = webdriver.Chrome(
             service=service,
             options=options
         )
 
+    def sendmessage(
+            self,
+            phone_number: str,
+            message: str,
+            instantly: bool = True
+    ) -> None:
         print('Initializing...')
-        driver.get('https://www.globo.com/')
+
+        if instantly:
+            self.driver.get(
+                'https://web.whatsapp.com/send?phone='
+                f'+{phone_number}&text={message}'
+                )
+
+            try:
+                send_button = WebDriverWait(self.driver, 60).until(
+                    EC.presence_of_element_located((
+                        By.XPATH, '/html/body/div[1]/div/div/div[5]/div/'
+                        'footer/div[1]/div/span[2]/div/div[2]/div[2]/button/'
+                        'span'
+                    ))
+                )
+                send_button.click()
+                print('Message sent')
+                time.sleep(.5)
+
+                print('Closing browser')
+                time.sleep(.5)
+                self.driver.quit()
+
+            except NoSuchElementException:
+                print('An error ocurred')
+                time.sleep(.5)
+                self.driver.quit()
 
 
-x = Zap('Profile 6', False)
+x = Pyzap('Profile 6', False)

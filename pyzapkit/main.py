ea7d9adc9e38061644392a0df18bc0c6dc073839
João Suzuki <joaosuzuki98@outlook.com>
2023-07-25 19:35:15 -0300

    Added an exception for the wrong chrome profile

--- pyzapkit/main.py
+++ pyzapkit/main.py
@@ -3,13 +3,14 @@ import os
 import platform
 import time
 import datetime
+from utils import exceptions
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 
 
 class Pyzap():
@@ -72,7 +73,7 @@ class Pyzap():
                 )
 
             try:
-                send_button = WebDriverWait(self.driver, 60).until(
+                send_button = WebDriverWait(self.driver, 20).until(
                     EC.presence_of_element_located((
                         By.XPATH, '/html/body/div[1]/div/div/div[5]/div/'
                         'footer/div[1]/div/span[2]/div/div[2]/div[2]/button/'
@@ -88,10 +89,11 @@ class Pyzap():
                 print('Closing browser')
                 time.sleep(.5)
                 self.driver.quit()
-            except NoSuchElementException:
-                print('An error ocurred')
-                time.sleep(.5)
-                self.driver.quit()
+            except TimeoutException as e:
+                raise exceptions.ChromeProfileException(
+                    'Wrong profile, you must choose a chrome profile '
+                    'that has your WhatsApp web account logged in'
+                ) from e
         else:
             target_time = f'{hour}:{min}'
             print(f'Message queued to {target_time}')
@@ -112,7 +114,7 @@ class Pyzap():
                         )
 
                     try:
-                        send_button = WebDriverWait(self.driver, 60).until(
+                        send_button = WebDriverWait(self.driver, 20).until(
                             EC.presence_of_element_located((
                                 By.XPATH, '/html/body/div[1]/div/div/div[5]/'
                                 'div/footer/div[1]/div/span[2]/div/div[2]/'
@@ -129,14 +131,13 @@ class Pyzap():
                         time.sleep(.5)
                         self.driver.quit()
                         break
-                    except NoSuchElementException:
-                        print('An error ocurred')
-                        time.sleep(.5)
-                        self.driver.quit()
-                        break
+                    except TimeoutException as e:
+                        raise exceptions.ChromeProfileException(
+                            'Wrong profile, you must choose a chrome profile '
+                            'that has your WhatsApp web account logged in'
+                        ) from e
 
                 time.sleep(1)
 
 
 x = Pyzap('Profile 6', False)
-x.sendmessage('5512991527926', 'Esta mensagem é automatizada')

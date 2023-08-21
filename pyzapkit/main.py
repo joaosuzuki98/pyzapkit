2fe95f5dd2c4a6ae90da8d054c8ee2f4772fdd96
João Suzuki <joaosuzuki98@outlook.com>
2023-08-21 18:45:18 -0300

    Added docstrings to the main module

--- pyzapkit/main.py
+++ pyzapkit/main.py
@@ -1,4 +1,12 @@
-"""Send WhatsApp messages"""
+"""
+Send WhatsApp messages, documents, videos and images
+====================================================
+
+Pyzapkit offers an automated way to send messages through Whatsapp.
+
+It contains methods to send images or videos, documents and normal text
+messages. It is possible to send them instantly or in a given time
+"""
 import os
 import platform
 import time
@@ -7,13 +15,30 @@ import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from utils import exceptions, browser_control
-from typing import Union
+from typing import Union, Callable
 
 
 class Pyzap(browser_control.BrowserControl):
+    """
+    Set the time in which the messages will be sent.
+
+    It stores time data in 24 format, phone number, message text or file path
+
+    :param profile: Google Chrome profile to be used
+    :type profile: str
+    :param headless: Activate Google Chrome headless mode
+    :type headless: bool
+    """
     def __init__(
             self, profile: str = 'Default', headless: bool = False) -> None:
+        """
+        Constructs the necessary atributes of the Pyzap class.       
 
+        :param profile: Google Chrome profile to be used
+        :type profile: str
+        :param headless: Activate Google Chrome headless mode
+        :type headless: bool
+        """
         self.profile = profile
         self.headless = headless
         op_sys = platform.system()
@@ -47,7 +72,25 @@ class Pyzap(browser_control.BrowserControl):
 
         self.options.add_argument(f'--profile-directory={self.profile}')
 
-    def _send(self, instantly, hour, min, func, *args):
+    def _send(
+            self, instantly: bool, hour: Union[int, str], 
+            min: Union[int, str], func: Callable, 
+            *args: Union[int, str]) -> None:
+        """
+        Set time for the message or file
+
+        :param instantly: Set if the message will be sent instantly or at a 
+        given time
+        :type instantly: bool
+        :param hour: The hour in which the message or file will be sent
+        :type hour: Union[int, str]
+        :param min: The minutes that will be used along the hours
+        :type min: Union[int, str]
+        :param func: A function that will set the sending type
+        :type func: Callable
+        :param *args: The necessary arguments for the func parameter
+        :type *args: Union[int, str]
+        """
         try:
             int_hour = int(hour)
             int_min = int(min)
@@ -90,6 +133,21 @@ class Pyzap(browser_control.BrowserControl):
             hour: Union[str, int] = '15',
             min: Union[str, int] = '00'
     ) -> None:
+        """
+        Send text messages to a given phone number
+
+        :param phone_number: The phone number that will receive the message
+        :type phone_number: Union[str, int]
+        :param message: The text message to be used in Whatsapp
+        :type message: Union[str, int]
+        :param instantly: If the message will be sent instantly or at a given 
+        time
+        :type instantly: bool
+        :param hour: The hour in which the message will be sent
+        :type hour: Union[int, str]
+        :param min: The minutes that will be used along the hours
+        :type min: Union[int, str]
+        """
         self._send(
             instantly, hour, min, self.browser_msg, phone_number, message,
             self.service, self.options)
@@ -103,6 +161,24 @@ class Pyzap(browser_control.BrowserControl):
         hour: Union[str, int] = '15',
         min: Union[str, int] = '00'
     ) -> None:
+        """
+        Send images or videos to a given phone number
+
+        :param phone_number: The phone number that will receive the file
+        :type phone_number: Union[str, int]
+        :param file_pathname: The file to be sent in Whatsapp
+        :type file_pathname: str
+        :param load_time: The time selenium will wait before closing the
+        browser, so the file sent can be uploaded
+        :type load_time: int
+        :param instantly: If the file will be sent instantly or at a given 
+        time
+        :type instantly: bool
+        :param hour: The hour in which the file will be sent
+        :type hour: Union[int, str]
+        :param min: The minutes that will be used along the hours
+        :type min: Union[int, str]
+        """
         self._send(
             instantly, hour, min, self.browser_img_vid, phone_number,
             file_pathname, load_time, self.service, self.options)
@@ -116,9 +192,27 @@ class Pyzap(browser_control.BrowserControl):
         hour: Union[str, int] = '15',
         min: Union[str, int] = '00'
     ) -> None:
+        """
+        Send document to a given phone number
+
+        :param phone_number: The phone number that will receive the document
+        :type phone_number: Union[int, str],
+        :param doc_pathname: The path to the document
+        :type doc_pathname: str
+        :param load_time: The time selenium will wait before closing the
+        browser, so the document sent can be uploaded
+        :type load_time: int
+        :param instantly: If the document will be sent instantly or at a given 
+        time
+        :type instantly: bool
+        :param hour: The hour in which the document will be sent
+        :type hour: Union[int, str]
+        :param min: The minutes that will be used along the hours
+        :type min: Union[int, str]
+        """
         self._send(
             instantly, hour, min, self.browser_doc, phone_number,
             doc_pathname, load_time, self.service, self.options)
 
 
-x = Pyzap()
+x = Pyzap('Profile 6')

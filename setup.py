d4520f1c8fa8b2d21dd05982c7b10ee39c0b55cf
João Suzuki <joaosuzuki98@outlook.com>
2023-09-09 21:47:52 -0300

    Updated setup

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import find_packages, setup
+from os import path
+
+
+root_path = path.abspath(path.dirname(__file__))
+
+with open(path.join(root_path, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='pyzapkit',
     packages=find_packages(include=['pyzapkit']),
     version='0.1.0',
     description='Automatically send Whatsapp messages and files',
+    long_description=long_description,
     author='João Suzuki',
     license='MIT',
     install_requires=[
